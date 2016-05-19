a122ab80f6d2c3f412baf0d7849c7c452d03904a
ItsMas <samvpearce@outlook.com>
2016-05-19 20:42:55 +0100

    Create HNRCore.java

---
+++ src/main/java/me/mas/hnrcore/HNRCore.java
@@ -0,0 +1,10 @@
+package me.mas.hnrcore;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class HNRCore extends JavaPlugin {
+
+    /*
+        Plugin 'HNRCore' created by ItsMas_ for the HNR Network
+    */
+}
